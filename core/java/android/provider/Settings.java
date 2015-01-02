0d155770c5ac8633cd30d84ea181050e988ecd7f
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-01-02 17:06:11 +0000

    SystemUI: Status bar carrier label (1/2)
    * allow choosing to show on keyguard, normal status bar or both
    * fix up some padding issues
    * remove "Emergency Calls Only" message when no sim is installed
    
    Signed-off-by: Altaf-Mahdi <altaf.mahdi@gmail.com>
    
    Change-Id: I11dfe9d1eb6a6d33d843f187f9425406f12cd14c
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/res/layout/status_bar.xml

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3462,6 +3462,20 @@ public final class Settings {
         public static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
 
         /**
+         * Status bar carrier label
+         * 0: Hide
+         * 1: Display on keyguard status bar
+         * 2: Display on Normal status bar
+         * 3: Enabled for both
+         * @hide
+         */
+        public static final String STATUS_BAR_SHOW_CARRIER = "status_bar_show_carrier";
+
+        /** @hide */
+        private static final Validator STATUS_BAR_SHOW_CARRIER_VALIDATOR =
+                new InclusiveIntegerRangeValidator(0, 3);
+
+        /**
          * Status bar battery %
          * 0: Hide the battery percentage
          * 1: Display the battery percentage inside the icon
@@ -3745,6 +3759,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(POINTER_SPEED);
             PRIVATE_SETTINGS.add(LOCK_TO_APP_ENABLED);
             PRIVATE_SETTINGS.add(EGG_MODE);
+            PRIVATE_SETTINGS.add(STATUS_BAR_SHOW_CARRIER);
         }
 
         /**
