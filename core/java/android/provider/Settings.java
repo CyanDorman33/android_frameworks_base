70f5ee1db704611940e325ebd3b713b4ac765e85
LorDClockaN <davor@losinj.com>
2015-08-28 20:59:06 +0200

    Toast icon switch [1/2]
    - Turn off by default
    
    Change-Id: I59ffd10f20a2ae9491f76380ec9af641e0844928
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3591,6 +3591,13 @@ public final class Settings {
         public static final String STATUS_BAR_CARRIER_COLOR = "status_bar_carrier_color";
 
         /**
+         * Toast icon
+         *
+         * @hide
+         */
+        public static final String TOAST_ICON = "toast_icon";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
