1ba2d4d744e3be0bff8a96d8a5bfe1f62aa6e4a0
John Brewer <gzroms@hotmail.com>
2015-10-29 21:51:31 -0400

    Update AlertDialog.java

--- core/java/android/app/AlertDialog.java
+++ core/java/android/app/AlertDialog.java
@@ -219,7 +219,7 @@ public class AlertDialog extends Dialog implements DialogInterface {
         } else if (themeResId == THEME_DEVICE_DEFAULT_LIGHT) {
             return R.style.Theme_DeviceDefault_Light_Dialog_Alert;
         } else if (themeResId == THEME_MATERIAL_DAYNIGHT) {
-            return R.style.Theme.Material.DayNight.Dialog.Alert;
+            return R.style.Theme_Material_DayNight_Dialog_Alert;
         } else if (themeResId >= 0x01000000) {   // start of real resource IDs.
             return themeResId;
         } else {
