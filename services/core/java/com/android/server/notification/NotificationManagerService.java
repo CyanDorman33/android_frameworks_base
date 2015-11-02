d757a2b97436b4893a44f3a4a497fe2ba0ed0d52
theringmaster <gzroms@hotmail.com>
2015-11-01 19:54:59 -0500

    Revert "Lights with Screen On [1/2]"
    This reverts commit 5dcb65dad8d09dfcd4c6e23fa9e64ad72b186abe.

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -220,9 +220,6 @@ public class NotificationManagerService extends SystemService {
     private int mDefaultNotificationLedOff;
     private long[] mDefaultVibrationPattern;
 
-    private boolean mScreenOnEnabled = false;
-    private boolean mScreenOnDefault = false;
-
     private long[] mFallbackVibrationPattern;
     private boolean mUseAttentionLight;
     boolean mSystemReady;
@@ -848,9 +845,6 @@ public class NotificationManagerService extends SystemService {
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.NOTIFICATION_LIGHT_PULSE_CUSTOM_VALUES),
                     false, this, UserHandle.USER_ALL);
-            resolver.registerContentObserver(Settings.System.getUriFor(
-                    Settings.System.NOTIFICATION_LIGHT_SCREEN_ON),
-                    false, this, UserHandle.USER_ALL);
             update(null);
         }
 
@@ -889,11 +883,6 @@ public class NotificationManagerService extends SystemService {
                         Settings.System.NOTIFICATION_LIGHT_PULSE_CUSTOM_VALUES,
                         UserHandle.USER_CURRENT));
             }
-
-            // Notification lights with screen on
-            mScreenOnEnabled = (Settings.System.getIntForUser(resolver,
-                    Settings.System.NOTIFICATION_LIGHT_SCREEN_ON,
-                    mScreenOnDefault ? 1 : 0, UserHandle.USER_CURRENT) != 0);
 
             updateNotificationPulse();
         }
@@ -3158,7 +3147,7 @@ public class NotificationManagerService extends SystemService {
             enableLed = false;
         } else if (isLedNotificationForcedOn(ledNotification)) {
             enableLed = true;
-        } else if (!mScreenOnEnabled && (mInCall || mScreenOn)) {
+        } else if (mInCall || mScreenOn) {
             enableLed = false;
         } else {
             enableLed = true;
